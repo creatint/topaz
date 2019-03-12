30967df7c18517a07379b329d1d6818a954868f4
Nick Korostelev <nkorsote@google.com>
2019-03-12 15:19:08 -0700

    [dart sdk] remove deprecated addServiceForName method
    Change-Id: I6aeb1acd02d774fc0c3abec5e4765e7f28ff58ef

--- public/dart/fuchsia_services/lib/src/outgoing.dart
+++ public/dart/fuchsia_services/lib/src/outgoing.dart
@@ -33,11 +33,6 @@ class Outgoing {
     }
   }
 
-  /// deprecated method, until we change all reference to use [addPublicService].
-  void addServiceForName<T>(vfs.Connector<T> connector, String serviceName) {
-    addPublicService(connector, serviceName);
-  }
-
   /// Serves root dir to request channel and serve [fuchsia.io.Directory]
   ///  over it.
   void serve(InterfaceRequest<Node> request) {
