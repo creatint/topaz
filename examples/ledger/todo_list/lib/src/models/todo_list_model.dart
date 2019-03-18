c1031c3260fd9421eb92f1a4074a0e3c37174162
Chase Latta <chaselatta@google.com>
2019-03-18 17:03:46 +0000

    [ledger] Update Todo List example
    Updates the Todo List example to use async
    bindings and fuchsia dart sdk.
    
    Change-Id: Ic7f0ac6608605ca60a21437774ee0ee35d4ecdcd

--- examples/ledger/todo_list/lib/src/models/todo_list_model.dart
+++ examples/ledger/todo_list/lib/src/models/todo_list_model.dart
@@ -2,27 +2,29 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:async';
 import 'dart:convert';
 import 'dart:math';
 import 'dart:typed_data' show Uint8List;
 
 import 'package:fidl/fidl.dart';
-import 'package:fidl_fuchsia_ledger/fidl.dart' as ledger;
-import 'package:fidl_fuchsia_modular/fidl.dart';
+import 'package:fidl_fuchsia_ledger/fidl_async.dart' as ledger;
+import 'package:fidl_fuchsia_modular/fidl_async.dart' as modular;
+import 'package:fuchsia_logger/logger.dart';
 import 'package:lib.widgets.dart/model.dart';
 
 import '../ledger_helpers.dart';
 
 /// The model for the todo list module.
-class TodoListModel extends Model implements ledger.PageWatcher {
-  final Random _random = new Random(new DateTime.now().millisecondsSinceEpoch);
+class TodoListModel extends Model {
+  final Random _random = Random(DateTime.now().millisecondsSinceEpoch);
 
   final ledger.PageWatcherBinding _pageWatcherBinding =
-      new ledger.PageWatcherBinding();
+      ledger.PageWatcherBinding();
 
-  final ledger.LedgerProxy _ledger = new ledger.LedgerProxy();
+  final ledger.LedgerProxy _ledger = ledger.LedgerProxy();
 
-  final ledger.PageProxy _page = new ledger.PageProxy();
+  final ledger.PageProxy _page = ledger.PageProxy();
 
   /// The todo items. The source of truth is Ledger, so [_items] should never be
   /// modified directly.
@@ -32,69 +34,53 @@ class TodoListModel extends Model implements ledger.PageWatcher {
   Map<List<int>, String> get items => _items;
 
   /// Call this method to connect the model
-  void connect(ComponentContextProxy componentContext) {
-    _ledger.ctrl.onConnectionError = () {
-      print('[Todo List] Ledger disconnected.');
-    };
-    componentContext.getLedger(
-      _ledger.ctrl.request(),
-    );
-    _ledger.getRootPageNew(_page.ctrl.request());
+  void connect(modular.ComponentContext componentContext) {
+    _ledger.ctrl.whenClosed.then((_) => log.warning('Ledger disconnected'));
+    componentContext.getLedger(_ledger.ctrl.request());
 
-    ledger.PageSnapshotProxy snapshot = new ledger.PageSnapshotProxy();
-    _page.getSnapshot(
+    _readInitialData();
+  }
+
+  Future<void> _readInitialData() async {
+    await _ledger.getRootPageNew(_page.ctrl.request());
+
+    final snapshot = ledger.PageSnapshotProxy();
+    final status = await _page.getSnapshot(
       snapshot.ctrl.request(),
       new Uint8List(0),
-      _pageWatcherBinding.wrap(this),
-      handleLedgerResponse('Watch'),
+      _pageWatcherBinding.wrap(_PageWatcher(this)),
     );
-
+    validateLedgerResponse(status, 'Watch');
     _readItems(snapshot);
   }
 
   /// Call when the module should be terminated.
-  void onTerminate() {
+  Future<void> onTerminate() async {
     _pageWatcherBinding.close();
     _ledger.ctrl.close();
     _page.ctrl.close();
   }
 
-  /// Implementation of PageWatcher.onChange().
-  @override
-  void onChange(ledger.PageChange pageChange, ledger.ResultState resultState,
-      void callback(InterfaceRequest<ledger.PageSnapshot> snapshotRequest)) {
-    if (resultState != ledger.ResultState.completed &&
-        resultState != ledger.ResultState.partialStarted) {
-      print(
-          '[Todo List] Unexpected result state in Ledger watcher: $resultState');
-      callback(null);
-      return;
-    }
-    ledger.PageSnapshotProxy snapshot = new ledger.PageSnapshotProxy();
-    callback(snapshot.ctrl.request());
-    _readItems(snapshot);
-  }
-
   /// Marks the item of the given [id] as done.
   void markItemDone(List<int> id) {
-    _page.delete(id, handleLedgerResponse('Delete'));
+    _page.delete(id).then((status) => validateLedgerResponse(status, 'Delete'));
   }
 
   /// Adds a new todo item with the given [content].
   void addItem(String content) {
-    _page.put(_makeKey(), utf8.encode(content), handleLedgerResponse('Put'));
+    _page
+        .put(_makeKey(), utf8.encode(content))
+        .then((status) => validateLedgerResponse(status, 'Put'));
   }
 
   void _readItems(ledger.PageSnapshotProxy snapshot) {
     getEntriesFromSnapshot(snapshot,
         (ledger.Status status, Map<List<int>, String> items) {
-      if (handleLedgerResponse('getEntries')(status)) {
-        return;
+      if (validateLedgerResponse(status, 'getEntries')) {
+        _items = items;
+        notifyListeners();
+        snapshot.ctrl.close();
       }
-
-      _items = items;
-      notifyListeners();
-      snapshot.ctrl.close();
     });
   }
 
@@ -104,5 +90,32 @@ class TodoListModel extends Model implements ledger.PageWatcher {
       key[i] = _random.nextInt(256);
     }
     return key;
+  }
+}
+
+class _PageWatcher extends ledger.PageWatcher {
+  final TodoListModel _model;
+
+  _PageWatcher(this._model);
+
+  /// Implementation of PageWatcher.onChange().
+  @override
+  Future<InterfaceRequest<ledger.PageSnapshot>> onChange(
+    ledger.PageChange pageChange,
+    ledger.ResultState resultState,
+  ) async {
+    if (resultState != ledger.ResultState.completed &&
+        resultState != ledger.ResultState.partialStarted) {
+      log.info('Unexpected result state in Ledger watcher: $resultState');
+      return null;
+    }
+    final snapshot = ledger.PageSnapshotProxy();
+
+    // need to call request() before _readItems so that the object
+    // is bound and the subsequent reads do not fail
+    final request = snapshot.ctrl.request();
+
+    _model._readItems(snapshot);
+    return request;
   }
 }
