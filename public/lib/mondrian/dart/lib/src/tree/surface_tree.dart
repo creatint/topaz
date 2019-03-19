eb6b8d5a88dff3f44abb18fe0bc401826eb6ef14
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2019-03-19 20:51:32 +0000

    [mondrian] Use Dart SDK fuchsia_logger
    Change-Id: I4be7127e96f0fa6a80db40b910b76c747c68fae8

--- public/lib/mondrian/dart/lib/src/tree/surface_tree.dart
+++ public/lib/mondrian/dart/lib/src/tree/surface_tree.dart
@@ -1,7 +1,7 @@
 // Copyright 2019 The Fuchsia Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import 'package:lib.app.dart/logging.dart';
+import 'package:fuchsia_logger/logger.dart';
 import 'package:meta/meta.dart';
 import '../surface/surface.dart';
 import '../surface/surface_relation.dart';
