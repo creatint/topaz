9a76f3c95a26eeea1898b2fac41f1c6fc6a82e6e
Ross Wang <rosswang@google.com>
2019-03-25 18:51:31 +0000

    [flutter_screencap_test] Modernizing flutter_screencap_test
    Adds hermetic variants of the flutter_screencap_test, using root presenter,
    dev basemgr, and sessionctl/Ermine.
    
    SCN-1244 #done
    
    Test: fx run-test flutter_screencap_test
    Change-Id: I2529aaed6ff40fd6d4b9656e8173fd3e0c03a33b

--- bin/flutter_screencap_test/lib/main.dart
+++ bin/flutter_screencap_test/lib/main.dart
@@ -2,21 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'dart:async';
-
-import 'package:fidl_fuchsia_sys/fidl.dart';
-import 'package:fidl_fuchsia_testing_runner/fidl.dart';
 import 'package:flutter/material.dart';
-import 'package:lib.app.dart/app.dart';
 import 'package:lib.app.dart/logging.dart';
 
 const Color _lightColor = const Color(0xFF4dac26);
 const Color _darkColor = const Color(0xFFd01c8b);
 const int _gridSize = 6;
-const String _testName = 'flutter_screencap';
-
-TestRunnerProxy runnerProxy;
-LauncherProxy launcherProxy;
 
 /// Display a checker board pattern in red and green to verify that the
 /// screen is displaying properly.
@@ -49,96 +40,11 @@ class CheckerBoard extends StatelessWidget {
 
 void main() {
   setupLogger(
-    name: 'flutter_screencap_test',
+    name: 'flutter_screencap_test_app',
     logToStdoutForTest: true,
   );
-  log.info('starting flutter screencap test');
-  StartupContext startupContext = new StartupContext.fromStartupInfo();
+  log.info('starting flutter_screencap_test_app');
   runApp(new MaterialApp(
     home: new CheckerBoard(),
   ));
-
-  _reportTestResultsIfInTestHarness(startupContext.environmentServices);
-}
-
-void _reportTestResultsIfInTestHarness(
-    ServiceProviderProxy environmentServices) {
-  log.warning('_reportTestResultsIfInTestHarness()');
-  runnerProxy = new TestRunnerProxy();
-  try {
-    connectToService(environmentServices, runnerProxy.ctrl);
-    runnerProxy.identify(_testName, () {});
-    try {
-      launcherProxy = new LauncherProxy();
-      connectToService(environmentServices, launcherProxy.ctrl);
-
-      runTestIterations();
-    } on Exception catch (e) {
-      log.warning('Not able to launch. Not enabling test mode: $e');
-      runnerProxy.teardown(() {
-        runnerProxy.ctrl.close();
-      });
-    }
-  } on Exception catch (e) {
-    log.warning('Not in automated test. Using normal mode: $e');
-  }
-}
-
-const int kMaxAttempts = 3;
-const int kDelayBeforeCaptureSeconds = 7;
-
-int _iterationAttempt = 0;
-
-void runTestIterations() {
-  log.info('runTestIterations()');
-  Stopwatch stopWatch = new Stopwatch()..start();
-  new Timer(const Duration(seconds: kDelayBeforeCaptureSeconds), () {
-    LaunchInfo launchInfo =
-        new LaunchInfo(url: 'fuchsia-pkg://fuchsia.com/screencap#meta/screencap.cmx', arguments: ['-histogram']);
-    final ComponentControllerProxy controller = new ComponentControllerProxy();
-    log.info('attempting to launch screencap');
-    launcherProxy.createComponent(launchInfo, controller.ctrl.request());
-    log.info('waiting for launch response');
-    controller.onTerminated = (int r, _) {
-      if (r == 0) {
-        TestResult testResult = new TestResult(
-          name: _testName,
-          elapsed: stopWatch.elapsedMilliseconds,
-          failed: false,
-          message: 'success',
-        );
-        launcherProxy.ctrl.close();
-        log.info('sending success test result');
-        runnerProxy
-          ..reportResult(testResult)
-          ..teardown(() {
-            runnerProxy.ctrl.close();
-          });
-        return;
-      } else {
-        _iterationAttempt++;
-        if (_iterationAttempt >= kMaxAttempts) {
-          log.info(
-              'iteration attempts exceeded elapsed: ${stopWatch.elapsedMilliseconds}');
-          TestResult testResult = new TestResult(
-              name: _testName,
-              elapsed: stopWatch.elapsedMilliseconds,
-              failed: true,
-              message: 'failed: See log for more info');
-          launcherProxy.ctrl.close();
-          log.info('sending failed test result');
-          runnerProxy
-            ..reportResult(testResult)
-            ..teardown(() {
-              runnerProxy.ctrl.close();
-            });
-          return;
-        }
-
-        // Try again
-        log.info('try again');
-        runTestIterations();
-      }
-    };
-  });
 }
