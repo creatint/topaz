0176301f4a80cff768ca22cb0faae3633687fc80
Bryce Lee <brycelee@google.com>
2019-03-26 18:29:07 +0000

    Revert "[base_shell] remove userpicker_base_shell, update ermine config"
    This reverts commit f709c204e5f9f8e4a3b309c13a92aec5bc923109.
    
    Reason for revert: breaks Ledger sync, which needs a signed in user
    
    Original change's description:
    > [base_shell] remove userpicker_base_shell, update ermine config
    >
    > Test: tried ermine
    >
    > Change-Id: Ib3c821a944fe411f701222f0a6eebc5bab53be0d
    
    TBR=ejia@google.com,sanjayc@google.com,alexmin@google.com,brycelee@google.com
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Change-Id: I38d88e3e08505b0879ab7229beb11d8b07b8f281

---
+++ bin/userpicker_base_shell/lib/authentication_overlay_model.dart
@@ -0,0 +1,77 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:fidl_fuchsia_ui_views/fidl_async.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:fuchsia_logger/logger.dart';
+import 'package:fuchsia_scenic_flutter/child_view_connection.dart'
+    show ChildViewConnection;
+import 'package:lib.widgets/model.dart';
+
+/// Manages the connection and animation of the authentication window.
+class AuthenticationOverlayModel extends Model implements TickerProvider {
+  ChildViewConnection _childViewConnection;
+  AnimationController _transitionAnimation;
+  CurvedAnimation _curvedTransitionAnimation;
+
+  /// Constructor.
+  AuthenticationOverlayModel() {
+    _transitionAnimation = new AnimationController(
+      value: 0.0,
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    _curvedTransitionAnimation = new CurvedAnimation(
+      parent: _transitionAnimation,
+      curve: Curves.fastOutSlowIn,
+      reverseCurve: Curves.fastOutSlowIn,
+    );
+  }
+
+  /// If not null, returns the handle of the current requested overlay.
+  ChildViewConnection get childViewConnection => _childViewConnection;
+
+  /// The animation controlling the fading in and out of the authentication
+  /// overlay.
+  CurvedAnimation get animation => _curvedTransitionAnimation;
+
+  /// Starts showing an overlay over all other content.
+  void onStartOverlay(ViewHolderToken overlayViewHolderToken) {
+    _childViewConnection = ChildViewConnection(
+      overlayViewHolderToken,
+      onAvailable: (ChildViewConnection connection) {
+        log.fine(
+          'AuthenticationOverlayModel: Child view connection available!',
+        );
+        _transitionAnimation.forward();
+        connection.requestFocus();
+      },
+      onUnavailable: (ChildViewConnection connection) {
+        log.fine(
+          'AuthenticationOverlayModel: Child view connection unavailable!',
+        );
+        _transitionAnimation.reverse();
+        // TODO(apwilson): Should not need to remove the child view
+        // connection but it causes a scenic deadlock in the compositor if you
+        // don't.
+        _childViewConnection = null;
+      },
+    );
+    notifyListeners();
+  }
+
+  /// Stops showing a previously started overlay.
+  void onStopOverlay() {
+    _transitionAnimation.reverse();
+    // TODO(apwilson): Should not need to remove the child view
+    // connection but it causes a scenic deadlock in the compositor if you
+    // don't.
+    _childViewConnection = null;
+    notifyListeners();
+  }
+
+  @override
+  Ticker createTicker(TickerCallback onTick) => new Ticker(onTick);
+}
