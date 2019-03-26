c26da3514a8bbc828621650afbd3cda40803637e
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2019-03-26 11:49:47 -0700

    [tiler] Get rid of padding
    TESTED=on device
    
    Change-Id: I3fa4636ca50dbacb7b91099f6c0b39bbc725fb67

--- lib/tiler/lib/src/tiler.dart
+++ lib/tiler/lib/src/tiler.dart
@@ -27,17 +27,14 @@ class Tiler extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(5),
-      child: AnimatedBuilder(
-        animation: model,
-        builder: (_, __) => Tile(
-              model: model.root,
-              chromeBuilder: chromeBuilder,
-              sizerBuilder: sizerBuilder,
-              sizerThickness: sizerThickness,
-            ),
-      ),
+    return AnimatedBuilder(
+      animation: model,
+      builder: (_, __) => Tile(
+            model: model.root,
+            chromeBuilder: chromeBuilder,
+            sizerBuilder: sizerBuilder,
+            sizerThickness: sizerThickness,
+          ),
     );
   }
 }
