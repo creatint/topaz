c81a8afa29c79d00758318644e5bb23a7317b436
Ross Wang <rosswang@google.com>
2019-03-28 21:27:46 +0000

    [driver_example_mod_tests] Removing delays and log level setting
    These are no longer necessary.
    
    Test: fx run-test driver_example_mod_tests -r 100
    Change-Id: I3fd578dd02729d6dd84fda6a4aff12c4f32cb4c9

--- examples/test/driver_example_mod/test/driver_example_mod_test.dart
+++ examples/test/driver_example_mod/test/driver_example_mod_test.dart
@@ -5,7 +5,6 @@
 import 'package:fidl/fidl.dart';
 import 'package:fidl_fuchsia_sys/fidl_async.dart';
 import 'package:flutter_driver/flutter_driver.dart';
-import 'package:fuchsia_remote_debug_protocol/logging.dart';
 import 'package:fuchsia_services/services.dart';
 import 'package:test/test.dart';
 
@@ -39,11 +38,6 @@ void main() {
     FlutterDriver driver;
 
     setUpAll(() async {
-      // TODO(DX-561): Update logging messages in
-      // fuchsia_remote_debug_protocol so that this doesn't need to be set to
-      // `all`.
-      Logger.globalLevel = LoggingLevel.all;
-
       await _startBasemgr(controller.ctrl.request(), _testAppUrl);
 
       driver = await FlutterDriver.connect(
@@ -63,23 +57,16 @@ void main() {
 
     test('add to counter. remove from counter', () async {
       await driver.tap(find.text('+1'));
-      await new Future<Null>.delayed(const Duration(milliseconds: 200));
       await driver.tap(find.text('+1'));
-      await new Future<Null>.delayed(const Duration(milliseconds: 200));
       await driver.tap(find.text('+5'));
-      await new Future<Null>.delayed(const Duration(milliseconds: 200));
       await driver.tap(find.text('-1'));
-      await new Future<Null>.delayed(const Duration(milliseconds: 200));
       SerializableFinder textFinder =
           find.text('This counter has a value of: 6');
       // If this value hasn't been set correctly the app will crash, as the
       // widget will not be findable.
       await driver.tap(textFinder);
-      await new Future<Null>.delayed(const Duration(milliseconds: 200));
       await driver.tap(find.text('-5'));
-      await new Future<Null>.delayed(const Duration(milliseconds: 200));
       await driver.tap(find.text('-1'));
-      await new Future<Null>.delayed(const Duration(milliseconds: 200));
       textFinder = find.text('This counter has a value of: 0');
       await driver.tap(textFinder);
     });
