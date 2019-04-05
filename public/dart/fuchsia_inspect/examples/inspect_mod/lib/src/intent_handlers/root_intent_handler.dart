5e99483dca769f9dce3b0e6a99d82348f35f5bab
Nina Chen <ninachen@google.com>
2019-04-05 20:36:32 +0000

    [inspect][dart] Correctly use topaz/public/dart/fuchsia_inspect/examples/inspect_mod/lib/src/inspect_example_app.dart
    Followup to https://fuchsia-review.googlesource.com/c/topaz/+/268329
    
    This change should have been part of that CL, but I must have had trouble using git and don't know what happened to it.
    
    CF-602 #comment
    
    Change-Id: I1d4720502c465410eb5ef8136bbf953f7c8b2a84

--- public/dart/fuchsia_inspect/examples/inspect_mod/lib/src/intent_handlers/root_intent_handler.dart
+++ public/dart/fuchsia_inspect/examples/inspect_mod/lib/src/intent_handlers/root_intent_handler.dart
@@ -7,30 +7,12 @@ import 'package:fuchsia_inspect/inspect.dart';
 import 'package:fuchsia_modular/module.dart';
 import 'package:fuchsia_services/services.dart';
 
+import '../inspect_example_app.dart';
+
 class RootIntentHandler extends IntentHandler {
   @override
   void handleIntent(Intent intent) {
     var context = StartupContext.fromStartupInfo();
-    runApp(_InspectExampleApp(Inspect(context)));
-  }
-}
-
-class _InspectExampleApp extends StatelessWidget {
-  static const _appColor = Colors.blue;
-
-  final Inspect _inspect;
-
-  _InspectExampleApp(this._inspect) {
-    _initMetrics();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(color: _appColor);
-  }
-
-  /// Initializes the [Inspect] metrics for this widget.
-  void _initMetrics() {
-    _inspect.rootNode.createStringProperty('app-color').value = '$_appColor';
+    runApp(InspectExampleApp(Inspect(context)));
   }
 }
