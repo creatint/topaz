8df9d102307ef68bd24c2168de9203dbe4d45cd0
Alexander Markov <alexmarkov@google.com>
2019-04-18 00:02:56 +0000

    [flutter_runner] Disable code garbage collection
    Change-Id: Idd3a1f859909acf76958ba974e7edcb374384d6a

--- runtime/flutter_runner/component.cc
+++ runtime/flutter_runner/component.cc
@@ -288,6 +288,10 @@ Application::Application(
   // addressed.
   settings_.dart_flags = {"--no_causal_async_stacks"};
 
+  // Disable code collection as it interferes with JIT code warmup
+  // by decreasing usage counters and flushing code which is still useful.
+  settings_.dart_flags.push_back("--no-collect_code");
+
   if (!flutter::DartVM::IsRunningPrecompiledCode()) {
     // The interpreter is enabled unconditionally in JIT mode. If an app is
     // built for debugging (that is, with no bytecode), the VM will fall back on
