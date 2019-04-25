4a82cc03796a0dbd1e68bcaafec75a2bd7b713b2
Ian McKellar <ianloic@google.com>
2019-04-25 20:52:14 +0000

    [dart][fidl_compatibility_test] Remove fd leak workarounds
    In particular:
     - stop killing the Dart runtimes before running the test
     - build against the default dart_app template rather than forcing AOT
    
    FL-240
    
    Change-Id: I79a1e209a8434cff4c494875cd8149b63466ece5

--- bin/fidl_compatibility_test/run_fidl_compatibility_test_topaz.sh
+++ bin/fidl_compatibility_test/run_fidl_compatibility_test_topaz.sh
@@ -4,13 +4,6 @@
 # garnet/bin/fidl_compatibility_test/run_fidl_compatibility_test_garnet.sh. (The
 # Garnet file should be similar to this file, but omit the Dart server.)
 
-
-### HACK HACK HACK HACK ###
-# The Dart runners currently leak file descriptors so let's restart them before
-# running this test.
-# This should be removed once FL-240 is fixed
-killall 'dart_*'
-
 run fuchsia-pkg://fuchsia.com/fidl_compatibility_test_bin#meta/fidl_compatibility_test_bin.cmx \
   fuchsia-pkg://fuchsia.com/fidl_compatibility_test_server_dart#meta/fidl_compatibility_test_server_dart.cmx \
   fuchsia-pkg://fuchsia.com/fidl_compatibility_test_server_cpp#meta/fidl_compatibility_test_server_cpp.cmx \
