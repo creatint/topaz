b300d6d699ffd3d17532d673329821387ff1e2eb
Chase Latta <chaselatta@google.com>
2019-04-26 21:35:46 +0000

    [runner] migrate integration tests to sdk
    This CL refactors the dart runner integration tests
    to use the dart sdk as well as async bindings.
    This CL also fixes the tests.
    
    TEST=
    - fx shell run 'fuchsia-pkg://fuchsia.com/dart_aot_runner_test#meta/dart_aot_runner_test.cmx'
    - fx shell run 'fuchsia-pkg://fuchsia.com/dart_jit_runner_test#meta/dart_jit_runner_test.cmx'
    
    Change-Id: I9c06bd1118d280a742fc36584203baf4c6806cb5

--- runtime/dart_runner/integration/main.dart
+++ runtime/dart_runner/integration/main.dart
@@ -5,51 +5,55 @@
 import 'dart:async';
 import 'dart:io' as io;
 
-import 'package:fidl_fuchsia_examples_hello/fidl.dart';
-import 'package:lib.app.dart/app.dart';
-import 'package:fidl_fuchsia_sys/fidl.dart';
+import 'package:fidl_fuchsia_examples_hello/fidl_async.dart';
+import 'package:fidl_fuchsia_io/fidl_async.dart';
+import 'package:fidl_fuchsia_sys/fidl_async.dart';
+import 'package:fuchsia_services/services.dart';
 import 'package:test/test.dart';
 
 void main(List<String> args) {
   final StartupContext context = StartupContext.fromStartupInfo();
-  tearDownAll(context.close);
+  LauncherProxy launcher;
+
+  setUp(() {
+    launcher = LauncherProxy();
+    context.incoming.connectToService(launcher);
+  });
+
+  tearDown(() {
+    launcher.ctrl.close();
+    launcher = null;
+  });
 
   // TODO(rosswang): nested environments and determinism
 
   test('schedule delayed futures',
       () => Future<Null>.delayed(const Duration(seconds: 1)));
 
-  test('start hello_dart', () {
+  test('start hello_dart', () async {
     const LaunchInfo info = LaunchInfo(
         url:
             'fuchsia-pkg://fuchsia.com/hello_dart_jit#meta/hello_dart_jit.cmx');
-    context.launcher.createComponent(info, null);
+    await launcher.createComponent(
+        info, ComponentControllerProxy().ctrl.request());
   });
 
   test('communicate with a fidl service (hello_app_dart)', () async {
-    final Services services = Services();
     final HelloProxy service = HelloProxy();
+    final dirProxy = DirectoryProxy();
 
     final ComponentControllerProxy actl = ComponentControllerProxy();
 
     final LaunchInfo info = LaunchInfo(
         url:
             'fuchsia-pkg://fuchsia.com/hello_app_dart_jit#meta/hello_app_dart_jit.cmx',
-        directoryRequest: services.request());
-    context.launcher.createComponent(info, actl.ctrl.request());
-    services
-      ..connectToService(service.ctrl)
-      ..close();
+        directoryRequest: dirProxy.ctrl.request().passChannel());
+    await launcher.createComponent(info, actl.ctrl.request());
+    Incoming(dirProxy).connectToService(service);
 
-    // TODO(rosswang): let's see if we can generate a future-based fidl dart
-    final Completer<String> hello = Completer<String>();
-    service.say('hello', hello.complete);
-
-    expect(await hello.future, equals('hola from Dart!'));
+    expect(await service.say('hello'), equals('hola from Dart!'));
 
     actl.ctrl.close();
-    expect(service.ctrl.error.timeout(const Duration(seconds: 2)),
-        throwsA(anything));
   });
 
   test('dart:io exit() throws UnsupportedError', () {
