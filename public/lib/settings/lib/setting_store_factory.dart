43f8c88c9554a2bb89e65d41058d285588aaef97
Nick Korostelev <nkorsote@google.com>
2019-04-29 19:15:59 +0000

    [cleanup] remove unused setting code
    Change-Id: I8e0996596fa7b371f3a1c8912d833492c6a1f354

--- public/lib/settings/lib/setting_store_factory.dart
+++ public/lib/settings/lib/setting_store_factory.dart
@@ -4,7 +4,7 @@
 
 import 'package:fidl_fuchsia_devicesettings/fidl_async.dart';
 import 'package:fuchsia_services/services.dart';
-import 'package:lib.app.dart/logging.dart';
+import 'package:fuchsia_logger/logger.dart';
 import 'package:settings_protos/audio.pb.dart';
 import 'package:settings_protos/setting_store.dart';
 
@@ -26,7 +26,6 @@ class SettingStoreFactory {
 
   /// Returns the setting store for [Audio].
   SettingStore<Audio> createAudioStore() {
-    return SettingStore<Audio>(
-        _deviceSettingsManagerService, 'Audio', Audio());
+    return SettingStore<Audio>(_deviceSettingsManagerService, 'Audio', Audio());
   }
 }
