e815667d2e8f06051f242e52f04785428bd8e743
Nick Korostelev <nkorsote@google.com>
2019-05-11 02:08:06 +0000

    [testing] start using new TestHarness in slider_mod_tests
    Start using Modular TestHarness instead of basemgr.
    
    DX-1249 #comment
    
    Test:
    fx run-test slider_mod_tests
    
    Change-Id: I2556bd8429bdad2e0fe537c2d5dc0bb6ee4842dd

--- public/dart/fuchsia_modular/examples/slider_mod/test/slider_mod_widget_test.dart
+++ public/dart/fuchsia_modular/examples/slider_mod/test/slider_mod_widget_test.dart
@@ -2,39 +2,99 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:fidl/fidl.dart';
+import 'package:fidl_fuchsia_modular_testing/fidl_async.dart';
 import 'package:fidl_fuchsia_sys/fidl_async.dart';
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:fuchsia_remote_debug_protocol/logging.dart';
 import 'package:fuchsia_services/services.dart';
 import 'package:test/test.dart';
+import 'package:fidl_fuchsia_modular/fidl_async.dart';
 
 const Pattern _isolatePattern = 'slider_mod.cmx';
 const _testAppUrl = 'fuchsia-pkg://fuchsia.com/slider_mod#meta/slider_mod.cmx';
-const _basemgrUrl = 'fuchsia-pkg://fuchsia.com/basemgr#meta/basemgr.cmx';
+const _modularTestHarnessURL =
+    'fuchsia-pkg://fuchsia.com/modular_test_harness#meta/modular_test_harness.cmx';
 
-// Starts basemgr with dev shells. This should be called from within a
-// try/finally or similar construct that closes the component controller.
-Future<void> _startBasemgr(
-    InterfaceRequest<ComponentController> controllerRequest,
-    String rootModUrl) async {
-  final context = StartupContext.fromStartupInfo();
+TestHarnessProxy testHarnessProxy = TestHarnessProxy();
+ComponentControllerProxy testHarnessController = ComponentControllerProxy();
+
+// Starts Modular TestHarness with dev shells. This should be called from within
+// a try/finally or similar construct that closes the component controller.
+Future<void> _startTestHarness() async {
   final launcher = LauncherProxy();
-  context.incoming.connectToService(launcher);
+  final incoming = Incoming();
 
-  final launchInfo = LaunchInfo(url: _basemgrUrl, arguments: [
-    '--base_shell=fuchsia-pkg://fuchsia.com/dev_base_shell#meta/dev_base_shell.cmx',
-    '--session_shell=fuchsia-pkg://fuchsia.com/dev_session_shell#meta/dev_session_shell.cmx',
-    '--session_shell_args=--root_module=$rootModUrl',
-    '--story_shell=fuchsia-pkg://fuchsia.com/dev_story_shell#meta/dev_story_shell.cmx',
-    '--test',
-    '--enable_presenter',
-    '--run_base_shell_with_test_runner=false'
-  ]);
-  await launcher.createComponent(launchInfo, controllerRequest);
-  launcher.ctrl.close();
+  // launch TestHarness component
+  StartupContext.fromStartupInfo().incoming.connectToService(launcher);
+  await launcher.createComponent(
+      LaunchInfo(
+          url: _modularTestHarnessURL,
+          directoryRequest: incoming.request().passChannel()),
+      testHarnessController.ctrl.request());
+
+  // connect to TestHarness service
+  incoming.connectToService(testHarnessProxy);
+
+  // helper function to convert a map of service to url into list of
+  // [InjectedService]
+  List<InjectedService> _toInjectedServices(Map<String, String> serviceToUrl) {
+    final injectedServices = <InjectedService>[];
+    for (final svcName in serviceToUrl.keys) {
+      injectedServices
+          .add(InjectedService(name: svcName, url: serviceToUrl[svcName]));
+    }
+    return injectedServices;
+  }
+
+  final testHarnessSpec = TestHarnessSpec(
+      envServicesToInherit: ['fuchsia.net.SocketProvider'],
+      envServicesToInject: _toInjectedServices(
+        {
+          'fuchsia.auth.account.AccountManager':
+              'fuchsia-pkg://fuchsia.com/account_manager#meta/account_manager.cmx',
+          'fuchsia.devicesettings.DeviceSettingsManager':
+              'fuchsia-pkg://fuchsia.com/device_settings_manager#meta/device_settings_manager.cmx',
+          'fuchsia.fonts.Provider':
+              'fuchsia-pkg://fuchsia.com/fonts#meta/fonts.cmx',
+          'fuchsia.sysmem.Allocator':
+              'fuchsia-pkg://fuchsia.com/sysmem_connector#meta/sysmem_connector.cmx',
+          'fuchsia.tracelink.Registry':
+              'fuchsia-pkg://fuchsia.com/trace_manager#meta/trace_manager.cmx',
+          'fuchsia.ui.input.ImeService':
+              'fuchsia-pkg://fuchsia.com/ime_service#meta/ime_service.cmx',
+          'fuchsia.ui.policy.Presenter':
+              'fuchsia-pkg://fuchsia.com/root_presenter#meta/root_presenter.cmx',
+          'fuchsia.ui.scenic.Scenic':
+              'fuchsia-pkg://fuchsia.com/scenic#meta/scenic.cmx',
+          'fuchsia.vulkan.loader.Loader':
+              'fuchsia-pkg://fuchsia.com/vulkan_loader#meta/vulkan_loader.cmx'
+        },
+      ));
+
+  // run the test harness which will create an encapsulated test env
+  await testHarnessProxy.run(testHarnessSpec);
 }
 
+Future<void> _launchModUnderTest() async {
+  // get the puppetMaster service from the encapsulated test env
+  final puppetMasterProxy = PuppetMasterProxy();
+  await testHarnessProxy.getService(
+      TestHarnessService.withPuppetMaster(puppetMasterProxy.ctrl.request()));
+  // use puppetMaster to start a fake story an launch the mod under test
+  final storyPuppetMasterProxy = StoryPuppetMasterProxy();
+  await puppetMasterProxy.controlStory(
+      'fooStoryName', storyPuppetMasterProxy.ctrl.request());
+  await storyPuppetMasterProxy.enqueue(<StoryCommand>[
+    StoryCommand.withAddMod(AddMod(
+        modName: ['slider_mod'],
+        modNameTransitional: 'root',
+        intent: Intent(action: 'action', handler: _testAppUrl),
+        surfaceRelation: SurfaceRelation()))
+  ]);
+  await storyPuppetMasterProxy.execute();
+}
+
+@Timeout(Duration(seconds: 10))
 void main() {
   final controller = ComponentControllerProxy();
   FlutterDriver driver;
@@ -48,7 +108,8 @@ void main() {
   setUpAll(() async {
     Logger.globalLevel = LoggingLevel.all;
 
-    await _startBasemgr(controller.ctrl.request(), _testAppUrl);
+    await _startTestHarness();
+    await _launchModUnderTest();
 
     // Creates an object you can use to search for your mod on the machine
     driver = await FlutterDriver.connect(
@@ -60,6 +121,9 @@ void main() {
   tearDownAll(() async {
     await driver?.close();
     controller.ctrl.close();
+
+    testHarnessProxy.ctrl.close();
+    testHarnessController.ctrl.close();
   });
 
   test(
