e4fc18f0f0615f812a4a20ab2df50a66fc6c8e25
Chase Latta <chaselatta@google.com>
2019-06-12 17:10:37 +0000

    [dart][vfs] add composed_pseudo_dir class
    TEST=
    - fx run-test fuchsia_vfs_package_unittests
    
    Change-Id: I98691b302a2ba2fc58da21175736da02c5460b96

--- public/dart/fuchsia_vfs/lib/vfs.dart
+++ public/dart/fuchsia_vfs/lib/vfs.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export 'src/composed_pseudo_dir.dart';
 export 'src/pseudo_dir.dart';
 export 'src/pseudo_file.dart';
 export 'src/service.dart';
