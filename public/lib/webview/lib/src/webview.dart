37feef123b842def17c57aa82e0a00a892cccc2d
Alon Hetzroni <ahetzroni@google.com>
2019-09-10 04:44:06 +0000

    [webview] update feature flags for ChromiumWebView
    BUG: 36012
    
    Change-Id: I934fcff7a30419a8ccd43e433a48a11a277f943a

--- public/lib/webview/lib/src/webview.dart
+++ public/lib/webview/lib/src/webview.dart
@@ -84,6 +84,10 @@ class ChromiumWebView {
     }
     final channel = Channel.fromFile('/svc');
     final web.CreateContextParams params = web.CreateContextParams(
+        features: web.ContextFeatureFlags.vulkan |
+            web.ContextFeatureFlags.hardwareVideoDecoder |
+            web.ContextFeatureFlags.audio |
+            web.ContextFeatureFlags.network,
         serviceDirectory: InterfaceHandle<fidl_io.Directory>(channel));
     contextProvider.create(params, context.ctrl.request());
     contextProvider.ctrl.close();
