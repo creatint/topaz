7c0727822d94f777618abc4cfd0658a41788d78f
Jean-François Geyelin <jif@google.com>
2019-12-17 14:38:39 +0100

    [sledge] Remove Sledge.
    Change-Id: Ia01755e56bd56c0df039e228022e7deb3a43cd58

--- public/dart/sledge/test/crdt_test_framework/crdt_test_framework.dart
+++
@@ -1,7 +0,0 @@
-// Copyright 2018 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-export 'checker.dart';
-export 'fleet.dart';
-export 'single_order_test_failure.dart';
