1fae115c6ffbb2dc4de7479f4d687dd1acfb87d7
Miguel Flores Ruiz de Eguino <miguelfrde@google.com>
2020-01-29 00:38:37 +0000

    [cleanup] Delete unused fuchsia_modular_flutter to stop depending on fuchsia.app.discover
    Change-Id: I2521ad923d3480218c27cf447de0c55624fe5120

--- public/dart/fuchsia_modular_flutter/lib/session_shell.dart
+++
@@ -1,9 +0,0 @@
-// Copyright 2019 The Fuchsia Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/// A collection of utilities simplifying Session Shell creation.
-export 'src/session_shell.dart';
-export 'src/session_shell_2.dart';
-export 'src/story.dart';
-export 'src/story_2.dart';
